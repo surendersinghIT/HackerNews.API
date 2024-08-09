b327bc4e9f7422a40e38355b5008f9b0169ed2ef
Surender Singh <surendersingh.it@gmail.com>
2024-08-09 14:59:39 +0530

    Updated test case

--- HackerNews.API.Tests/IntegrationTests/IntegrationTests.cs
+++ HackerNews.API.Tests/IntegrationTests/IntegrationTests.cs
@@ -14,25 +14,24 @@ namespace HackerNews.API.Tests.IntegrationTests
         }
 
         /// <summary>
-        /// Test method to get top 200 new stories from API
-        /// This method can be failed if Hack news api dosn't 
-        /// return exact 200 stories
+        /// Test method to get top upto 200 new stories from API
         /// </summary>
         /// <returns></returns>
         [TestMethod]
-        public async Task HackerNews_FetchNewStories_Returns200Stories()
+        public async Task HackerNews_FetchNewStories_ReturnsMax200Stories()
         {
             // Arrange
             var client = _webFactory.CreateClient();
 
             // Act
             var response = await client.GetAsync("/api/newstories");
-
+            var responseData = await response.Content.ReadAsStringAsync();
+            
             // Assert
-            List<Story>? responseContent = JsonSerializer.Deserialize<List<Story>>(await response.Content.ReadAsStringAsync());
+            List<Story>? responseContent = JsonSerializer.Deserialize<List<Story>>(responseData);
 
             Assert.IsNotNull(responseContent);
-            Assert.AreEqual(200, responseContent.Count);
+            Assert.IsTrue(200 >= responseContent.Count, "Total Number of new stories are not more than 200");
         }
     }
 }
